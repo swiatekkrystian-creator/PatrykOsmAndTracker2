40b145a52adaf88de23ca886e1992d7ec0fe8c84
swiatekkrystian-creator <swiatekkrystian@gmail.com>
2026-08-31 18:26:27 +0400

    Fix ktfmt formatting for HTTP error response logging

--- project/app/src/main/java/org/owntracks/android/net/http/HttpMessageProcessorEndpoint.kt
+++ project/app/src/main/java/org/owntracks/android/net/http/HttpMessageProcessorEndpoint.kt
@@ -124,7 +124,9 @@ class HttpMessageProcessorEndpoint(
           if (!response.isSuccessful) {
             val responseBody = response.body?.string().orEmpty()
             val httpException = Exception("HTTP request failed. Status: ${response.code}")
-            Timber.e("HTTP request failed. Status: ${response.code}; Server response: ${responseBody.take(4000)}")
+            Timber.e(
+                "HTTP request failed. Status: ${response.code}; Server response: ${responseBody.take(4000)}",
+            )
             endpointStateRepo.setState(
                 EndpointState.ERROR.withMessage(
                     String.format(Locale.ROOT, "HTTP code %d", response.code),
